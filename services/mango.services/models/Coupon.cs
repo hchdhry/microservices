95550370745264ee22e43f0800f3edc5f9db7e8d
hassan <hchoudhury20@hotmail.com>
2024-08-25 14:11:25 +0100

    create coupon class

---
+++ services/mango.services/models/Coupon.cs
@@ -0,0 +1,9 @@
+public class coupon
+{
+    public int couponID {get;set;}
+    public string CouponCode {get;set;}
+
+    public double DiscountAmount{get;set;}
+
+    public int MinAmount{get;set;}
+}
