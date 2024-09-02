fedf5a04f90fad653b4f7f3cb856144eb694aaad
hassan <hchoudhury20@hotmail.com>
2024-09-02 14:39:19 +0100

    implement response dto

--- services/mango.services.CouponAPI/Controllers/CouponController.cs
+++ services/mango.services.CouponAPI/Controllers/CouponController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using mango.services.Data;
 using Mango.Services.CouponAPI.Models;
 using Microsoft.EntityFrameworkCore;
+using mango.services.models.DTOs;
+
 
 namespace mango.services.Controllers
 {
@@ -11,58 +13,66 @@ namespace mango.services.Controllers
     public class CouponController : ControllerBase
     {
         private readonly ApplicationDBContext _DB;
-        public CouponController(ApplicationDBContext dBContext)
+        private readonly ResponseDTO _responseDTO;
+        public CouponController(ApplicationDBContext dBContext, ResponseDTO responseDTO)
         {
             _DB = dBContext;
+            _responseDTO = responseDTO;
 
         }
         [HttpGet]
 
-        public async Task<IEnumerable<coupon>> Get()
+        public async Task<ResponseDTO> Get()
         {
             try
             {
-              IEnumerable<coupon> coupons = await _DB.Coupons.ToListAsync(); 
-              return coupons; 
+              _responseDTO.Result = await _DB.Coupons.ToListAsync(); 
+              
             }
             catch(Exception e)
             {
-                Console.WriteLine(e.Message);
-                return Enumerable.Empty<coupon>();
+               _responseDTO.Message = e.Message;
+               _responseDTO.Result = Enumerable.Empty<coupon>;
 
             }
+            return _responseDTO;
         }
         
         [HttpGet]
         [Route("id:int")]
-        public async Task<coupon> Get(int id)
+        public async Task<ResponseDTO> Get(int id)
         {
             try
             {
-                coupon coupon = await _DB.Coupons.FirstAsync(u=>u.couponID == id);
-                return coupon;
+                _responseDTO.Result = await _DB.Coupons.FirstAsync(u=>u.couponID == id);
+                _responseDTO.isSuccess = true;
+                
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                return null;
+                _responseDTO.Message = e.Message;
 
             }
+            return _responseDTO;
         }
+        
         [HttpPost]
-        public async Task<coupon> Post([FromBody] coupon coupon)
+        [Route("create")]
+        public async Task<ResponseDTO> Post([FromBody] coupon coupon)
         {
             try
             {
                 await _DB.Coupons.AddAsync(coupon);
                 await _DB.SaveChangesAsync();
-                return coupon;
+                _responseDTO.isSuccess = true;
+                
             }
             catch (Exception e)
             {   
-                Console.WriteLine(e);
-                return null;
+              
+                _responseDTO.Message = e.Message;
             }
+            return _responseDTO;
         }
 
 
--- services/mango.services.CouponAPI/Program.cs
+++ services/mango.services.CouponAPI/Program.cs
@@ -1,14 +1,16 @@
 using mango.services.Data;
+using mango.services.models.DTOs;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<ResponseDTO>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
