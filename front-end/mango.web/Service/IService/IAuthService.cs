5e606c215314c7509119e90cccca42916db2e5da
hassan <hchoudhury20@hotmail.com>
2024-10-15 17:17:11 +0100

    create interface for auth service

---
+++ front-end/mango.web/Service/IService/IAuthService.cs
@@ -0,0 +1,13 @@
+using System;
+using mango.web.models.DT;
+using mango.web.models.DTO;
+
+namespace mango.web.Service.IService;
+
+public interface IAuthService
+{
+    Task<ResponseDTO> LoginAsync(LoginDTO loginDTO);
+    Task<ResponseDTO> RegisterAsync(RegisterDTO registerDTO);
+    Task<ResponseDTO> AssignRoleAsync(RegisterDTO registerDTO);
+
+}
