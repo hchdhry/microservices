a68a56a7ee22f5b0d429fc30b6ecf062fc7cc340
hassan <hchoudhury20@hotmail.com>
2024-10-25 12:47:09 +0100

    create token provider service

---
+++ front-end/mango.web/Service/TokenProvider.cs
@@ -0,0 +1,20 @@
+using mango.web.Service.IService;
+
+namespace mango.web.Service;
+public class TokenProvider : ITokenProvider
+{
+    public void ClearToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string? GetToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetToken(string token)
+    {
+    
+    }
+}
