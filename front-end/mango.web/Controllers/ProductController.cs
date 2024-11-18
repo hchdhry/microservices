9dbc2bacbdaba91f47757066b55841fa32742a97
hassan <hchoudhury20@hotmail.com>
2024-11-18 16:39:15 +0000

    implement product service

--- front-end/mango.web/Controllers/ProductController.cs
+++ front-end/mango.web/Controllers/ProductController.cs
@@ -2,22 +2,30 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using mango.web.Models;
 using mango.web.models.DTO;
+using mango.web.Service.IService;
+using Newtonsoft.Json;
 
 namespace mango.web.Controllers;
 
 public class ProductController : Controller
 {
-    
+    private readonly IProductService _productService;
 
-    public ProductController()
+    public ProductController(IProductService productService)
     {
-        
+        _productService = productService;
     }
 
-    public IActionResult Index()
+
+    public async Task<IActionResult> Index()
     {
-        
-        return View();
+        List<ProductDTO> productDTOs = new();
+        ResponseDTO response = await _productService.GetAllProductsAsync();
+        if (response != null && response.isSuccess)
+        {
+            productDTOs = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+        }
+        return View(productDTOs);
     }
 
    
---
+++ front-end/mango.web/Service/ProductService.cs
@@ -0,0 +1,44 @@
+using System;
+using mango.web.models.DTO;
+using mango.web.Models.DTO;
+using mango.web.Utilities;
+
+namespace mango.web.Service.IService;
+
+public class ProductService : IProductService
+{
+    private readonly IBaseService _baseService;
+    public ProductService(IBaseService baseService)
+    {
+        _baseService = baseService;
+
+    }
+    public Task<ResponseDTO> CreateProduct(ProductDTO productDTODTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDTO> DeleteCoupon(int ProductDTO)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<ResponseDTO> GetAllProductsAsync()
+    {
+        return await _baseService.SendAsync(new RequestDTO
+        {
+            APItype = SD.APIType.GET,
+            Url = SD.ProductAPIURL + "/api/Product",
+        }, WithBearer: false);
+    }
+
+    public Task<ResponseDTO> GetProductByIdAsync(int ProductId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseDTO> UpdateCoupon(ProductDTO productDTO)
+    {
+        throw new NotImplementedException();
+    }
+}
