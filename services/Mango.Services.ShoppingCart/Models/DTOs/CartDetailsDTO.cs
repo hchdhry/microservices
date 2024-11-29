ab169e900684abc3d141d7620ea785c504118ecc
hassan <hchoudhury20@hotmail.com>
2024-11-29 14:09:30 +0000

    create dto for shopping cart

---
+++ services/Mango.Services.ShoppingCart/Models/DTOs/CartDetailsDTO.cs
@@ -0,0 +1,15 @@
+
+using Mango.Services.ShoppingCart.Models.DTOs;
+
+namespace Mango.Services.ShoppingCart.Models.Dto
+{
+    public class CartDetailsDto
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public CartHeaderDto? CartHeader { get; set; }
+        public int ProductId { get; set; }
+        public ProductDTO? Product { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ services/Mango.Services.ShoppingCart/Models/DTOs/ResponseDTO.cs
@@ -0,0 +1,9 @@
+namespace Mango.Services.ShoppingCart.Models.DTOs
+{
+    public class ResponseDTO
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
